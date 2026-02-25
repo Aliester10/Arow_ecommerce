4dde7fc46ec91376e70f62632c6d343e6e74f849
Aliester10 <kisjo.official0411@gmail.com>
2026-02-25 22:22:38 +0700

    update migration

--- database/migrations/2026_02_24_054508_create_integrated_solutions_table.php
+++ database/migrations/2026_02_24_054508_create_integrated_solutions_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,7 +14,7 @@ return new class extends Migration
             $table->id();
             $table->string('title')->default('Integrated Solutions for Modern Businesses');
             $table->string('background_image')->nullable();
-            $table->text('button_text')->default('See Now');
+            $table->string('button_text')->default('See Now');
             $table->string('button_color')->default('#FF5F57');
             $table->boolean('is_active')->default(true);
             $table->timestamps();
